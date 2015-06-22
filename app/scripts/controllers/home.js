295a91a550662e4bd28d26bf38b11996b8717dde
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-22 12:44:33 -0400

    Added submit directive for user to hit Enter and changed readme.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,11 +8,14 @@
  * Controller of the forecastApp
  */
 angular.module('forecastApp')
-  .controller('HomeCtrl', function($scope, cityService) {
+  .controller('HomeCtrl', function($scope, $location, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function(){
       cityService.city = $scope.city;
     });
+    $scope.submit = function() {
+      $location.path('/forecast');
+    };
     // This watcher allows for us to make data available from other views.
   });
 
